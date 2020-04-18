87c2d662b33538ae4ebe1c54ce6796f3e9be3558
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-04-19 02:53:48 +0530

    Initial Commit

---
+++ screenie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  screenie
+//
+//  Created by Mahesh Prasad on 19/04/20.
+//  Copyright © 2020 CreatesApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
